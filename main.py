6ca4357335a620a6075850b21f7b05426f23f50c
Kavin Shanbhag <kavinplays@icloud.com>
2021-06-05 12:15:43 +0000

    buttons test

--- main.py
+++ main.py
@@ -457,7 +457,11 @@ async def testbuttons(ctx):
    res = await bot.wait_for("button_click") #Wait for button to be clicked
    await res.respond(type=InteractionType.ChannelMessageWithSource, content=f'Button Clicked') #Responds to the button click by printing out a message only user can see #In our case, its "Button Clicked"
 
-client
+@bot.command()
+async def buttons(ctx):
+   await ctx.channel.send("Context", components = [Button(style=ButtonStyle.blue, label="Test")]) #Blue button with button label of "Test"
+   res = await bot.wait_for("button_click") #Wait for button to be clicked
+   await res.respond(type=InteractionType.ChannelMessageWithSource, content=f'Button Clicked')
 bot.run(os.environ['TOKEN'])
 
 # apply mongodb on all code
